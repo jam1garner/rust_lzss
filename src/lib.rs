111e1032ff13c07cf4592f79df6fd36fc7e7b7c5
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-05-01 19:03:58 -0400

    Remove unneeded unsafe

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ pub fn decompress<T: Read + Seek>(data: &mut T) -> Result<Vec<u8>, Error> {
     let lz_type = data.read_u8()?;
     let mut data_size_tmp: [u8; 4] = [0; 4];
     data.read_exact(&mut data_size_tmp[0..3])?;
-    let data_size = unsafe { transmute::<[u8; 4], u32>(data_size_tmp) };
+    let data_size = u32::from_le_bytes(data_size_tmp);
 
     match lz_type {
         0x10 => decompress_lzss10(data, data_size as usize),
